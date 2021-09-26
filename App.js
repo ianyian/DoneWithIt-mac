f0344b8c47d41a0d30ec411d5ab8d3d4ba153d46
ianyian <ianyian@gmail.com>
2021-09-26 16:23:48 +0800

    update

--- App.js
+++ App.js
@@ -1,24 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  let x = 1;
-  console.log("App executed");
-
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!x!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
